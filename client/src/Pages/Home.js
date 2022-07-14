920cb9013a7c2c4fdac2aa37560424dd936d1110
Aryan Bakshi <91716389+aryan-bakshii@users.noreply.github.com>
2022-07-15 04:14:17 +0530

    feat: implement cookies
    preserve logged in sessions until logged out

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,43 +1,57 @@
 import React from "react"
 import jwtDecode from "jwt-decode"
 import { useNavigate } from "react-router-dom"
-import {Box} from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 export default function Home() {
 
-  const [user, setUser] = React.useState({})
-  const navigate = useNavigate()
   function handleCallbackResponse(response) {
     var userObject = jwtDecode(response.credential)
     setUser(userObject)
+    localStorage.setItem("user", JSON.stringify(userObject))
+    console.log(userObject)
     navigate('/form', { state: userObject })
   }
+
+  const [user, setUser] = React.useState({})
+  const navigate = useNavigate()
+
+
   React.useEffect(() => {
     /* global google */
-    google.accounts.id.initialize({
-      client_id: "1050493483344-ardfhdeca71u0v4758micitopt027jnr.apps.googleusercontent.com",
-      callback: handleCallbackResponse
-    })
+    if (localStorage.getItem("user") === null) {
+      google.accounts.id.initialize({
+        client_id: "1050493483344-ardfhdeca71u0v4758micitopt027jnr.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+      })
 
-    google.accounts.id.renderButton(
-      document.getElementById("signInDiv"),
-      {
-        theme: "outline", size: "large"
-      }
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        {
+          theme: "outline", size: "large"
+        }
 
-    )
-    google.accounts.id.prompt();
+      )
+      google.accounts.id.prompt();
+    }
+    if (localStorage.getItem("user") !== null) {
+      setUser(localStorage.getItem('user'))
+      navigate('/form', { state: JSON.parse(localStorage.getItem("user")) })
+    }
+
+    // to logout just run localstorage.clear(); and navigate to /
+
   }, [])
 
-//   return (
-//     <div id="Home">
-//       <div id="signInDiv"></div>
-//     </div>)
-// }
+  //   return (
+  //     <div id="Home">
+  //       <div id="signInDiv"></div>
+  //     </div>)
+  // }
 
-return(
-  <Box id = "Home" h="100vh" bg = "beige">
-    <Box id = "signInDiv">
+  return (
+    <Box id="Home" h="100vh" bg="beige">
+      <Box id="signInDiv">
+      </Box>
     </Box>
-  </Box>
-)
+  )
 }
