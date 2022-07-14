a8caae2decdbf86ba97893fa4f82b0e125295efa
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2022-07-14 16:45:12 +0530

    refactor: add fonts and import chakra

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react"
 import Form from "./Pages/Form"
 import Home from "./Pages/Home"
-import { BrowserRouter, Routes, Route, Redirect} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react";
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/form" element={<Form/>} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
--- client/src/Pages/Form.js
+++ client/src/Pages/Form.js
@@ -1,32 +1,43 @@
 import React from "react"
 import { useLocation } from "react-router-dom"
 import axios from "axios";
+import { chakra, Box } from "@chakra-ui/react"
+import { Gilmer } from "../gilmer-complete-family-webfont-full/gilmer-bold/webfonts/gilmer-bold.ttf"
 export default function Form() {
     const location = useLocation()
     const data = location.state;
-    console.log(data);
     const [formData, setFormData] = React.useState({
         firstName: data.given_name,
         lastName: data.family_name,
-        img: data.picture,
+        img: {},
         quote: "",
         id: "",
         email: data.email
     })
+    // let formInfo = new FormData();
+
     function handleChange(event) {
+        const { name, value, type } = event.target
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                [name]: (type == "file" ? event.target.files[0] : value)
             }
         })
     }
+    //  function handleFile(e) {
+    //     console.log(e.target.files[0])
+    //     if(e.target && e.target.files[0]){
+    //         formInfo.append('file', e.target.files[0])
+    //     }
+    // }
     function handleSubmit(e) {
         e.preventDefault();
+        // console.log(formData)
         axios({
             method: 'post',
             url: 'http://127.0.0.1:3001/profile/add',
-            data: formData
+            data: { formData }
         })
             .then(function (response) {
                 console.log(response);
@@ -37,15 +48,22 @@ export default function Form() {
 
     }
     return (
-        <div className="form">
-            <form onSubmit={handleSubmit}>
-                <input onChange={handleChange} name="firstName" type="text" value={formData.firstName} />
-                <input onChange={handleChange} name="lastName" type="text" value={formData.lastName} />
-                <input onChange={handleChange} name="img" type="image" src={formData.img} />
-                <input onChange={handleChange} name="quote" type="text" value={formData.quote} placeholder="Quote" />
-                <input onChange={handleChange} name="id" type="text" value={formData.id} placeholder="BITS ID" />
-                <button>Submit</button>
-            </form>
-        </div>
+        <Box>
+            <Box>
+                <Box bg="#1D1E22" w="80%" color="white" fontFamily={Gilmer}>
+                    <form onSubmit={handleSubmit}>
+                        <input onChange={handleChange} name="firstName" type="text" value={formData.firstName} />
+                        <input onChange={handleChange} name="lastName" type="text" value={formData.lastName} />
+                        <input onChange={handleChange} name="img" type="file" accept="image/*" />
+                        <input onChange={handleChange} name="quote" type="text" value={formData.quote} placeholder="Quote" />
+                        <input onChange={handleChange} name="id" type="text" value={formData.id} placeholder="BITS ID" />
+                        <button>Submit</button>
+                    </form>
+                </Box>
+            </Box>
+            <Box>
+
+            </Box>
+        </Box>
     )
 }
