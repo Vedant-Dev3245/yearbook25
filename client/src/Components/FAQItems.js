e293b4081d42ef322650caf92abd65ba7d3176e8
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2022-07-31 13:36:27 +0530

    fix: fix elements in home

--- client/src/Components/FAQItems.js
+++ client/src/Components/FAQItems.js
@@ -16,12 +16,12 @@ function CollapseEx() {
 const FAQItems = (props) => {
     const [isSmallerThan800] = useMediaQuery('(max-width:800px)')
     return (
-        <Box justifyContent="space-between" pt={5} pl={10} pr={10} pb={5} borderRadius={8} border="3px solid rgba(240, 248, 255, 0.25);" width='90%' onClick={() => props.setFaq(!props.show)} cursor="pointer">
+        <Box zIndex={"2"} justifyContent="space-between" pt={5} pl={10} pr={10} pb={5} borderRadius={8} border="3px solid rgba(240, 248, 255, 0.25);" width='90%' onClick={() => props.setFaq(!props.show)} cursor="pointer">
             
             <Flex justifyContent="space-between">
                 <Image src="../images/bullet.png" mt="0.6rem" w="1rem" h="1rem" ml={isSmallerThan800 ? "-1.2rem" : 0} />
                 <Flex justifyContent="space-between" w="95%">
-                    <Text fontWeight={500} opacity={1} fontSize={isSmallerThan800 ? "1rem" : "1.2rem"} fontStyle="Poppins" color="#fff" opacity={1} position='relative'>{props.faq}</Text>
+                    <Text fontWeight={500} opacity={1} fontSize={isSmallerThan800 ? "1rem" : "1.2rem"} fontStyle="Poppins" color="#fff" position='relative'>{props.faq}</Text>
                     {props.show ? <Image mt="0.4rem" w={isSmallerThan800 ? "1rem" : "1.2rem"} h={isSmallerThan800 ? "1rem" : "1.2rem"} src="../images/arrowDown.png" transform='rotate(180deg)' /> : <Image mt="0.4rem" w={isSmallerThan800 ? "1rem" : "1.2rem"} h={isSmallerThan800 ? "1rem" : "1.2rem"} src="../images/arrowDown.png" />}
                 </Flex>
             </Flex>
--- client/src/Components/OptIn.js
+++ client/src/Components/OptIn.js
@@ -13,6 +13,7 @@ export default function OptIn(){
             bg={isSmallerThan800?"url('../images/rectangle.png')":"url('../images/Rectangle 39.png')"} 
             backgroundSize="cover" mt="7rem" className="physical" 
             position="relative" top="4rem"
+            borderRadius="50px"
             mb={100}>
                 <VStack
                     w="80%"
@@ -26,7 +27,7 @@ export default function OptIn(){
                     backdrop-filter="blur(40px)"
                     z-index="2"
                     paddingBottom="2.5rem"
-                    border-radius="50px" >
+                     >
                     <Box fontSize={isSmallerThan800 ? "1.6rem" : "3rem"} fontWeight="700" mt={isSmallerThan800?"3rem":"6rem"}>opt-in for the <Box display="inline" fontStyle="italic" fontFamily="EB Garamond">physical</Box> yearbook 🥂</Box>
                     <Box color="#C9C8C8" mt={isSmallerThan800 ? "-1rem" : 0} fontSize={isSmallerThan800 ? "1rem" : "1.4rem"}>get physical copy. cherish it for the years to come.</Box>
                     <Button className="phyButton" marginBottom={isSmallerThan800?"2rem !important":"6rem !important"} fontSize="1.2rem" color="black" fontWeight="700">yes, i do need yearbook irl <ArrowForwardIcon />
