0096d29de53f5f689267fedfa17d0d46698e58ac
Shwetabh Niket <f20210923@pilani.bits-pilani.ac.in>
2022-08-30 16:04:18 +0530

    enable cors on localhost

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(
 
 //app.use(passport.initialize());
 //app.use(passport.session());
-var whitelist = ['https://sarc-yearbook-sarc.vercel.app','https://yearbook.bits-sarc.org'];
+var whitelist = ['http://localhost:3000/','https://sarc-yearbook-sarc.vercel.app','https://yearbook.bits-sarc.org'];
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
