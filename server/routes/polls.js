dff516d54dd79c278f6e672272c7d78dbd847a4e
Himanshu Kumar <kumarh664@gmail.com>
2023-06-17 19:31:23 +0530

    tab size to 4 spaces

--- server/routes/polls.js
+++ server/routes/polls.js
@@ -7,23 +7,23 @@ const { User, Search } = require("../models/user");
 
 //getall(user and admin)
 router.route("/").get(async (req, res) => {
-  const questions = await Poll.find({}).select(["ques", "total_count"]);
-  return res.status(200).json({ questions });
+    const questions = await Poll.find({}).select(["ques", "total_count"]);
+    return res.status(200).json({ questions });
 });
 
 //getone(user and admin)
 router.route("/:id").get(async (req, res) => {
-  try {
-    const { id: pollID } = req.params;
-    const poll = await Poll.findById(pollID);
-    if (!poll) {
-      return res.status(404).json({ msg: `No poll found by the ID ${pollID}` });
+    try {
+        const { id: pollID } = req.params;
+        const poll = await Poll.findById(pollID);
+        if (!poll) {
+            return res.status(404).json({ msg: `No poll found by the ID ${pollID}` });
+        }
+        res.status(200).json({ poll });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "something went wrong" });
     }
-    res.status(200).json({ poll });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "something went wrong" });
-  }
 });
 
 // {
@@ -31,20 +31,20 @@ router.route("/:id").get(async (req, res) => {
 // }
 //create(admin)
 router.route("/create").post(async (req, res) => {
-  try {
-    const users_all = await User.find({});
-    const ques = req.body.question;
-    const poll = await Poll.create({
-      ques: ques,
-      vote: users_all.map((user) => {
-        return { User: user.email, count: 0, is_ans: false };
-      }),
-    });
-    res.status(201).json({ poll });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ msg: "something went wrong" });
-  }
+    try {
+        const users_all = await User.find({});
+        const ques = req.body.question;
+        const poll = await Poll.create({
+            ques: ques,
+            vote: users_all.map((user) => {
+                return { User: user.email, count: 0, is_ans: false };
+            }),
+        });
+        res.status(201).json({ poll });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ msg: "something went wrong" });
+    }
 });
 
 // {
@@ -52,39 +52,39 @@ router.route("/create").post(async (req, res) => {
 // }
 //update(admin)
 router.route("/update/:id").patch(async (req, res) => {
-  try {
-    const { id: pollID } = req.params;
-    const question = req.body.question;
-    const poll = await Poll.findByIdAndUpdate(
-      pollID,
-      { ques: question },
-      { new: true, runValidators: true }
-    );
-    if (!poll) {
-      return res.status(404).json({ msg: `No poll found with ID ${pollID}` });
+    try {
+        const { id: pollID } = req.params;
+        const question = req.body.question;
+        const poll = await Poll.findByIdAndUpdate(
+            pollID,
+            { ques: question },
+            { new: true, runValidators: true }
+        );
+        if (!poll) {
+            return res.status(404).json({ msg: `No poll found with ID ${pollID}` });
+        }
+        res.status(200).json({ poll });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ msg: "something went wrong" });
     }
-    res.status(200).json({ poll });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ msg: "something went wrong" });
-  }
 });
 
 //delete(admin)
 router.route("/delete/:id").delete(async (req, res) => {
-  try {
-    const { id: pollID } = req.params;
-    const poll = await Poll.findByIdAndDelete(pollID);
-    if (!poll) {
-      return res
-        .status(404)
-        .json({ msg: `No poll found with the id ${pollID}` });
+    try {
+        const { id: pollID } = req.params;
+        const poll = await Poll.findByIdAndDelete(pollID);
+        if (!poll) {
+            return res
+                .status(404)
+                .json({ msg: `No poll found with the id ${pollID}` });
+        }
+        res.status(200).json({ msg: `Poll with ID ${pollID} deleted` });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "something went wrong" });
     }
-    res.status(200).json({ msg: `Poll with ID ${pollID} deleted` });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "something went wrong" });
-  }
 });
 
 // {
@@ -94,43 +94,43 @@ router.route("/delete/:id").delete(async (req, res) => {
 
 //post(User)-to ans the poll
 router.route("/vote/:id").patch(async (req, res) => {
-  try {
-    const user_main = req.body.user_main;
-    const to_vote = req.body.user_to_vote;
-    const { id: pollID } = req.params;
-    const poll = await Poll.findById(pollID);
-    const total = poll.total_count;
-    console.log(total);
-    if (user_main === to_vote) {
-      return res.status(400).json({ msg: "You cannot vote for yourself" });
+    try {
+        const user_main = req.body.user_main;
+        const to_vote = req.body.user_to_vote;
+        const { id: pollID } = req.params;
+        const poll = await Poll.findById(pollID);
+        const total = poll.total_count;
+        console.log(total);
+        if (user_main === to_vote) {
+            return res.status(400).json({ msg: "You cannot vote for yourself" });
+        }
+        if (!poll) {
+            return res
+                .status(404)
+                .json({ msg: `Cannot find a poll with the ID ${pollID}` });
+        }
+        if (!user_main || !to_vote) {
+            return res
+                .status(400)
+                .json({ msg: "Send both the user mail and the person voted" });
+        }
+        if (poll.vote.find((user) => user.User === user_main)["is_ans"] === false) {
+            const poll_updated = await Poll.findById(pollID).then((doc) => {
+                doc.total_count = total + 1;
+                main_user = doc.vote.find((user) => user.User === user_main);
+                to_vote_user = doc.vote.find((user) => user.User === to_vote);
+                to_vote_user["count"] = to_vote_user["count"] + 1;
+                main_user["is_ans"] = true;
+                doc.save();
+            });
+        } else {
+            return res.status(400).json({ msg: "You have already voted" });
+        }
+        res.status(200).json({ msg: "successfullupdated" });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ msg: "somethong went wrong" });
     }
-    if (!poll) {
-      return res
-        .status(404)
-        .json({ msg: `Cannot find a poll with the ID ${pollID}` });
-    }
-    if (!user_main || !to_vote) {
-      return res
-        .status(400)
-        .json({ msg: "Send both the user mail and the person voted" });
-    }
-    if (poll.vote.find((user) => user.User === user_main)["is_ans"] === false) {
-      const poll_updated = await Poll.findById(pollID).then((doc) => {
-        doc.total_count = total + 1;
-        main_user = doc.vote.find((user) => user.User === user_main);
-        to_vote_user = doc.vote.find((user) => user.User === to_vote);
-        to_vote_user["count"] = to_vote_user["count"] + 1;
-        main_user["is_ans"] = true;
-        doc.save();
-      });
-    } else {
-      return res.status(400).json({ msg: "You have already voted" });
-    }
-    res.status(200).json({ msg: "successfullupdated" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ msg: "somethong went wrong" });
-  }
 });
 
 module.exports = router;
