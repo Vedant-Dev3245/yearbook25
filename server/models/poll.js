aa33849490e79c739be93eba00fe428e889ead76
Rakshit2622 <rakshit.sakhuja345@gmail.com>
2023-06-20 01:14:55 +0530

    added brach in polls

--- server/models/poll.js
+++ server/models/poll.js
@@ -2,31 +2,35 @@ const mongoose = require("mongoose");
 const User = require("./user");
 
 const PollSchema = new mongoose.Schema({
-    ques: {
+  ques: {
+    type: String,
+    required: [true, "Ques field cannot be empty"],
+    maxlength: [300, "Ques cannot be greater than 300 characters"],
+  },
+  total_count: {
+    type: Number,
+    default: 0,
+  },
+  branch: {
+    type: String,
+    required: [true, "Branch id must be provided"],
+  },
+  vote: [
+    {
+      User: {
         type: String,
-        required: [true, "Ques field cannot be empty"],
-        maxlength: [300, "Ques cannot be greater than 300 characters"],
-    },
-    total_count: {
+        required: true,
+      },
+      count: {
         type: Number,
         default: 0,
+      },
+      is_ans: {
+        type: Boolean,
+        default: false,
+      },
     },
-    vote: [
-        {
-            User: {
-                type: String,
-                required: true,
-            },
-            count: {
-                type: Number,
-                default: 0,
-            },
-            is_ans: {
-                type: Boolean,
-                default: false,
-            },
-        },
-    ],
+  ],
 });
 
 const Poll = mongoose.model("Poll", PollSchema);
