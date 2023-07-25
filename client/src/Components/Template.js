399954c9023d31db3ccb05e14d4f1114e0fe22d8
JaiVR <f20221264@pilani.bits-pilani.ac.in>
2023-07-25 13:11:30 +0530

    ints story testing

--- client/src/Components/Template.js
+++ client/src/Components/Template.js
@@ -1,9 +1,16 @@
 import React from "react";
-import "./Template.css";
+
 
 export default function Template(props){
+     
+    const templateStyle={
+        backgroundColor : "linear-gradient(138deg, #050505 0%, #07111B 24.96%, #130D1F 46.35%, #130C1E 79.69%, #020202 89.58%);"
+    }
+
+
     return(
-        <div className="template">
+        <div
+        style={templateStyle}>
             <h1>{props.caption}hello my boii its time to rock</h1>
         </div>
     )
