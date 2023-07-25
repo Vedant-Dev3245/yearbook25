f64f1e1f303141d39498db8d5dbc5fec49fc520b
JaiVR <f20221264@pilani.bits-pilani.ac.in>
2023-07-25 13:23:26 +0530

    insta testing

--- client/src/Components/Template.js
+++ client/src/Components/Template.js
@@ -3,14 +3,14 @@ import React from "react";
 
 export default function Template(props){
      
-    const templateStyle={
-        backgroundColor : "linear-gradient(138deg, #050505 0%, #07111B 24.96%, #130D1F 46.35%, #130C1E 79.69%, #020202 89.58%);"
-    }
+    // const templateStyle={
+    //     backgroundColor : "linear-gradient(138deg, #050505 0%, #07111B 24.96%, #130D1F 46.35%, #130C1E 79.69%, #020202 89.58%);"
+    // }
 
 
     return(
         <div
-        style={templateStyle}>
+      >
             <h1>{props.caption}hello my boii its time to rock</h1>
         </div>
     )
