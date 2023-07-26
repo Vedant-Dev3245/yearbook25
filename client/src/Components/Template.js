a9f393f952bb4311ef43aaf02991a1b39c69d8c5
JaiVR <f20221264@pilani.bits-pilani.ac.in>
2023-07-26 23:08:12 +0530

    testing

--- client/src/Components/Template.js
+++ client/src/Components/Template.js
@@ -1,17 +1,12 @@
+// In the Template component
 import React from "react";
 
-
-export default function Template(props){
-     
-    // const templateStyle={
-    //     backgroundColor : "linear-gradient(138deg, #050505 0%, #07111B 24.96%, #130D1F 46.35%, #130C1E 79.69%, #020202 89.58%);"
-    // }
-
-
-    return(
-        <div
-      >
-            <h1>{props.caption}hello my boii its time to rock</h1>
+const Template = ({ caption }) => {
+    return (
+        <div className="template">
+            <p>{caption}</p>
         </div>
-    )
-}
+    );
+};
+
+export default Template;
