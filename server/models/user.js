83b1d7f22942cbfe8aa28661ef1a14f42f2c613b
prithvi05prism <prithvi05prism@gmail.com>
2024-07-04 15:48:35 +0530

    created models for user and captions

--- server/models/user.js
+++ server/models/user.js
@@ -1,73 +1,83 @@
-const mongoose = require('mongoose')
-//this file determines the user model and schema
-var UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        uppercase: true
-    },
-    imageUrl: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    personalEmail: {
-        type: String
-    },
-    phone: {
-        type: String
-    },
-    bitsId: {
-        type: String,
-        required: true
-    },
-    nominatedby: { // the user is nominated by who
-        type: Array
-    },
-    quote: {
-        type: String
-    },
-    branchCode: [{
-        type: String,
-        required: true
-    }],
-    captions: [{
-        user: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "User",
-            required: true
+const {Sequelize, DataTypes} = require("sequelize");
+const sequelize = new Sequelize("postgres");
+
+const User = sequelize.define(
+    'User', 
+    {
+        user_id:{
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey: true
         },
-        caption: { type: String, required: true }
-    }],
-    requests: [{
-        user: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "User",
-            required: true
+
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
         },
-        caption: { type: String, required: true }
-    }],
-    declined_requests: [{
-        user: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "User",
-            required: true
+
+        imageUrl:{
+            type: DataTypes.STRING
         },
-        caption: { type: String, required: true }
-    }],
-})
 
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
 
-// UserSchema.index({name: 'text'})
-var User = mongoose.model('User', UserSchema);
+        personalEmail:{
+            type: DataTypes.STRING
+        },
 
-const searchSchema = new mongoose.Schema({
-    uId: String,
-    name: String,
-    bitsId: String
-});
-searchSchema.index({ name: "text", bitsId: "text" })//adding name as an index for searching the Search collection
-var Search = mongoose.model('Search', searchSchema);
+        phone:{
+            type: DataTypes.STRING
+        },
 
-module.exports = { User, Search }
+        bitsId: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        nominatedby: { // the user is nominated by who
+            type: DataTypes.ARRAY(DataTypes.STRING)
+        },
+
+        quote: {
+            type: DataTypes.STRING
+        },
+
+        branchCode: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        commitments:{
+            type: DataTypes.ARRAY(DataTypes.UUID)
+        },
+
+        requests: {
+            type: DataTypes.ARRAY(DataTypes.UUID)
+        },
+
+        declined_requests: {
+            type: DataTypes.ARRAY(DataTypes.UUID)
+        }
+    }
+);
+
+// IMPLEMENT THE BELOW CODE IN SEQUELIZE - SEARCHING FOR USERS THROUGH THEIR NAME OR ID:
+
+// // UserSchema.index({name: 'text'})
+// var User = mongoose.model('User', UserSchema);
+
+// const searchSchema = new mongoose.Schema({
+//     uId: String,
+//     name: String,
+//     bitsId: String
+// });
+// searchSchema.index({ name: "text", bitsId: "text" })//adding name as an index for searching the Search collection
+// var Search = mongoose.model('Search', searchSchema);
+
+// module.exports = { User, Search }
+
+module.exports = {User};
