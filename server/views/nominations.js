fcb58c32ad826efa5b5e0fc404603fe2929b98d5
prithvi05prism <prithvi05prism@gmail.com>
2024-07-08 00:41:00 +0530

    nomination routes updated

--- server/views/nominations.js
+++ server/views/nominations.js
@@ -1,6 +1,7 @@
-const { User, Search } = require("../models/user");
+const { User } = require("../models/user");
 const Filter = require("bad-words");
 const words = require("../bad-words.json");
+const { request } = require("express");
 // const sgMail = require("@sendgrid/mail");
 // sgMail.setApiKey(process.env.SENDGRID);
 
@@ -8,6 +9,7 @@ const sendRequest = async (req, res) => {
   try {
     const senderId = req.user.id;
     const targetId = req.body.targetId;
+
     var caption = req.body.caption;
 
     if (senderId == targetId) {
@@ -17,8 +19,10 @@ const sendRequest = async (req, res) => {
       });
     }
 
-    const sender = await User.findById(senderId);
-    const target = await User.findById(targetId);
+    const session = User.transaction();
+
+    const sender = await User.findByPk(senderId);
+    const target = await User.findByPk(targetId);
 
     const filter = new Filter({ placeHolder: "x" });
     filter.addWords(...words);
@@ -29,49 +33,46 @@ const sendRequest = async (req, res) => {
       target.declined_requests.find((o) => o.user == senderId) ||
       target.nominatedby.find((o) => o.user == senderId)
     ) {
-      return res.send({
+      await session.rollback();
+      console.log("Request has already been sent by" + senderId + " to " + targetId);
+      return res.status(500).send({
         status: "failure",
         msg: "You have already sent a request to this user",
       });
     }
 
-    await target.updateOne({
-      $push: {
-        requests: {
-          $each: [
-            {
-              user: senderId,
-              caption: caption,
-            },
-          ],
-        },
-      },
-    });
+    // Check if array methods need to be changed to Raw SQL Queries
+    await target.requests.push({"user": senderId, "caption": caption});
+    target.save({transaction: session});
 
-    const mailOptions = {
-      from: "studentalumnirelationscell@gmail.com",
-      to: "receiverEmail",
-      subject: "Online Yearbook Portal",
-      // change the email test from here
-      html: `<p>Greetings from the Student Alumni Relations Cell! <br>
-                          ${sender.name} has requested to write on your yearbook wall! <br>
-                          <br>
-                          Log on to the <a href="yearbook.bits-sarc.in">yearbook portal</a> to accept their request. <br>
-                          <br>
-                          Regards,
-                          Student Alumni Relations Cell! <br>
-                           </p>`,
-    };
+    session.commit();
 
-    // TODO:
-    // Send mail
+    // !!! IS THIS deprecated? 
+    // // TODO:
+    // // Send mail
+    // const mailOptions = {
+    //   from: "studentalumnirelationscell@gmail.com",
+    //   to: "receiverEmail",
+    //   subject: "Online Yearbook Portal",
+    //   // change the email test from here
+    //   html: `<p>Greetings from the Student Alumni Relations Cell! <br>
+    //                       ${sender.name} has requested to write on your yearbook wall! <br>
+    //                       <br>
+    //                       Log on to the <a href="yearbook.bits-sarc.in">yearbook portal</a> to accept their request. <br>
+    //                       <br>
+    //                       Regards,
+    //                       Student Alumni Relations Cell! <br>
+    //                        </p>`,
+    // };
 
     return res.send({
       status: "success",
       msg: "Request sent successfully!",
     });
+
   } catch (err) {
-    console.log(err);
+    console.log("There was an erorr: ", err);
+
     return res.send({
       status: "failure",
       msg: "There was an error, Please try after some time",
@@ -81,10 +82,8 @@ const sendRequest = async (req, res) => {
 
 const allRequests = async (req, res) => {
   try {
-    senderId = req.user.id;
-    sender = await User.findById(senderId)
-      .populate("requests.user", "name id")
-      .populate("declined_requests", "name id");
+    const senderId = req.user.id;
+    const sender = await User.findByPk(senderId)
 
     return res.send({
       status: "success",
@@ -99,21 +98,22 @@ const allRequests = async (req, res) => {
   }
 };
 
+
 const nominateUser = async (req, res) => {
   try {
     const senderId = req.user.id;
     const receiverId = req.body.receiverId;
 
-    const session = await User.startSession();
-    session.startTransaction();
+    const session = await User.transaction();
+  
+    const receiver = await User.findByPk(receiverId, {transaction: session});
+    const sender = await User.findByPk(senderId, {transaction: session});
 
-    const receiver = await User.findById(receiverId).session(session);
-    const sender = await User.findById(senderId).session(session);
     const senderName = sender.name;
-
     const receiverEmail = receiver.email;
+
     if (receiver.nominatedby.find((obj) => obj.id == senderId)) {
-      session.endSession();
+      session.rollback();
       return res.send({
         status: "failure",
         msg: "User has already been nominated!",
@@ -130,95 +130,81 @@ const nominateUser = async (req, res) => {
     // Handle requests
     // if the target user had requested to write on the wall
     // remove request and add it to nominated
+
     if (sender.requests.find((o) => o.user == receiverId)) {
       const requests = sender.requests;
       let newCap;
       for (let i = 0; i < requests.length; i++) {
         if (requests[i].user == receiverId) {
           newCap = requests[i].caption;
+
           const filter = new Filter({ placeHolder: "x" });
           filter.addWords(...words);
           newCap = filter.clean(newCap);
+
           requests.splice(i, 1);
         }
       }
-      await sender
-        .updateOne({
-          $push: {
-            captions: {
-              $each: [
-                {
-                  user: receiver,
-                  caption: newCap,
-                },
-              ],
-            },
-          },
-        })
-        .session(session);
-      await sender.updateOne({ requests }).session(session);
+      try{
+
+        sender.captions.push({"user": receiver, "caption": newCap});
+        sender.requests = requests;
+
+        await sender.save({transaction: session});
+
+      }catch(err){
+        console.log("Some error occured ", err);
+        session.rollback();
+        return res.status(500).send({"message": "An error occured"});
+      }
+
     } else if (sender.declined_requests.find((o) => o.user == receiverId)) {
       const declined_requests = sender.declined_requests;
       let newCap;
       for (let i = 0; i < declined_requests.length; i++) {
         if (declined_requests[i].user === receiverId) {
           newCap = declined_requests[i].caption;
+
+          const filter = new Filter({ placeHolder: "x" });
+          filter.addWords(...words);
+          newCap = filter.clean(newCap);
+          
           declined_requests.splice(i, 1);
         }
       }
-      await sender
-        .updateOne({
-          $push: {
-            captions: {
-              $each: [
-                {
-                  user: receiver,
-                  caption: newCap,
-                },
-              ],
-            },
-          },
-        })
-        .session(session);
-      await sender.updateOne({ declined_requests }).session(session);
+      
+      sender.captions.push({"user": receiver, "caption": newCap});
+      sender.declined_requests = declined_requests;
+    
+      await sender.save({transaction: session});
+
     }
+      
+    receiver.nominatedby.push({"name": senderName, "id": senderId});
+    receiver.save({transaction: session});
 
-    await receiver
-      .updateOne({
-        $push: {
-          nominatedby: {
-            $each: [
-              {
-                name: senderName,
-                id: senderId,
-              },
-            ],
-          },
-        },
-      })
-      .session(session);
-    await session.commitTransaction();
-    session.endSession();
-    const mailOptions = {
-      from: "studentalumnirelationscell@gmail.com",
-      to: receiverEmail,
-      subject: "Online Yearbook Portal",
-      // change the email test from here
-      html: `<p>Greetings from the Student Alumni Relations Cell! <br>
-                            You have been nominated by <b>${senderName}</b> to write a caption for their yearbook.<br>
-                            Please keep the following points in mind while writing the captions:-<br>
-                            <ol>
-                            <li> There is a no-limit rule to the captions you can write about your friends! </li>
-                            <li> You can modify the caption before the deadline through the notifications tab. </li>   
-                            <li> Please refrain from using any expletives or Devanagari script while writing captions. </li>
-                            </ol>
-                            Login at yearbook.bits-sarc.in to enter the caption under the notifications tab  <br>
-                            <br>
-                            Regards,
+    await session.commit();
 
-                            Student Alumni Relations Cell! <br>
-                            </p>`,
-    };
+    // const mailOptions = {
+    //   from: "studentalumnirelationscell@gmail.com",
+    //   to: receiverEmail,
+    //   subject: "Online Yearbook Portal",
+    //   // change the email test from here
+    //   html: `<p>Greetings from the Student Alumni Relations Cell! <br>
+    //                         You have been nominated by <b>${senderName}</b> to write a caption for their yearbook.<br>
+    //                         Please keep the following points in mind while writing the captions:-<br>
+    //                         <ol>
+    //                         <li> There is a no-limit rule to the captions you can write about your friends! </li>
+    //                         <li> You can modify the caption before the deadline through the notifications tab. </li>   
+    //                         <li> Please refrain from using any expletives or Devanagari script while writing captions. </li>
+    //                         </ol>
+    //                         Login at yearbook.bits-sarc.in to enter the caption under the notifications tab  <br>
+    //                         <br>
+    //                         Regards,
+
+    //                         Student Alumni Relations Cell! <br>
+    //                         </p>`,
+    // };
 
     return res.send({
       status: "success",
@@ -253,32 +239,23 @@ const declineRequest = async (req, res) => {
     senderId = req.user.id;
     receiverId = req.body.receiverId;
 
-    sender = await User.findById(senderId);
-    receiver = await User.findById(receiverId);
+    sender = await User.findByPk(senderId);
+    receiver = await User.findByPk(receiverId);
 
     const requests = sender.requests;
     let newCap;
+
     for (let i = 0; i < requests.length; i++) {
       if (requests[i].user === receiver) {
         newCap = requests[i].caption;
         requests.splice(i, 1);
       }
     }
-    await receiver
-      .updateOne({
-        $push: {
-          declined_requests: {
-            $each: [
-              {
-                user: receiver,
-                caption: newCap,
-              },
-            ],
-          },
-        },
-      })
-      .session(session);
-    await sender.updateOne({ requests }).session(session);
+    
+    await receiver.declined_requests.push({"user": receiver, "caption": newCap});
+    sender.requests = requests;
+    receiver.save();
+    sender.save();
 
     return res.send({
       success: "Succesfully declined",
@@ -290,5 +267,6 @@ const declineRequest = async (req, res) => {
     });
   }
 };
+
 
 module.exports = { allRequests, nominateUser, declineRequest, sendRequest };
