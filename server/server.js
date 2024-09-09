7a921c9d5f9ebaa4fa0ae8593548f3ac7f13bcf6
prithvi05prism <prithvi05prism@gmail.com>
2024-09-09 19:39:10 +0530

    cors stuff

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.use(
 app.use(cors());
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
