81a65ce535573a1a6130708861542831880d9651
prithvi05prism <prithvi05prism@gmail.com>
2024-09-26 20:30:19 +0530

    fix: commitments permissions

--- server/routes/commitments.js
+++ server/routes/commitments.js
@@ -9,11 +9,11 @@ const {
     deleteCommitment
 } = require("../views/commitments");
 
-router.post("/", addCommitment);
+router.post("/", isAdmin, addCommitment);
 router.get("/", allCommitments);
 router.post("/user", updateUserCommitments);
-router.delete("/:id", deleteCommitment);
+router.delete("/:id", isAdmin, deleteCommitment);
 router.get("/search", searchByCommitment);
-router.put("/", editCommitment);
+router.put("/", isAdmin, editCommitment);
 
 module.exports = router;
